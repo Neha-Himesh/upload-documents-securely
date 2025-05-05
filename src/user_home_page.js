4edb58467a075ba47470e6575e63137437f89977
Neha-Himesh <neha.himesh@spike.sh>
2025-05-05 12:34:02 +0530

    Added common css page for navbar and edited username related elements

--- src/user_home_page.js
+++ src/user_home_page.js
@@ -5,7 +5,7 @@ import { signOut } from 'firebase/auth';
 document.addEventListener('DOMContentLoaded', () => {
 	const userData = JSON.parse(sessionStorage.getItem('userData'));
     const logoutBtn = document.getElementById('logout');
-
+	console.log(logoutBtn);
 	if (!userData) {
 		alert("Session expired. Please log in again.");
 		window.location.href = 'index.html';
@@ -15,13 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	console.log("Logged-in user:", userData.name);
 
 	// Update the sidebar or any element with the user's name
-	const loggedInUserName = document.getElementsByClassName('user-home-page-username-display');
-	if (loggedInUserName.length) {
-		for(var i = 0; i < loggedInUserName.length; i ++){
-			loggedInUserName[i].textContent = userData.name;
-		}
-		
-	}
+	const loggedInUserNameInNavbar = document.querySelector('.user-home-page-username-display-in-navbar');
+	const loggedInUserNameInWelcomeElement = document.querySelector('.user-home-page-username-display-in-welcome-element');
+	loggedInUserNameInNavbar.textContent = userData.name ;
+	loggedInUserNameInWelcomeElement.textContent = "Hi " + userData.name + ",";
+	
     logoutBtn.addEventListener('click', async () => {
         try {
             await signOut(auth);                      // Firebase logout
